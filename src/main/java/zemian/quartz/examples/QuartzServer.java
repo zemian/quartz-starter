5a12e6af0e88aa82b2dccf1630028e0baa3e8c10
Zemian Deng <zemiandeng@gmail.com>
2017-07-04 11:36:45 -0400

    Add shutdownhook in quartz server

--- src/main/java/zemian/quartz/examples/QuartzServer.java
+++ src/main/java/zemian/quartz/examples/QuartzServer.java
@@ -1,6 +1,7 @@
 package zemian.quartz.examples;
 
 import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
 import org.quartz.impl.StdSchedulerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,16 +12,25 @@ import org.slf4j.LoggerFactory;
 public class QuartzServer {
     private static Logger LOG = LoggerFactory.getLogger(QuartzServer.class);
     public static final String DEFAULT_CONFIG = "zemian/quartz/examples/quartz.properties";
+    
     public static void main(String[] args) throws Exception {
-        String config = System.getProperty("quartzConfig",DEFAULT_CONFIG);
-        Scheduler scheduler = new StdSchedulerFactory(config).getScheduler();
-        try {
-            LOG.info("Starting quartz server.");
-            scheduler.start();
-            Thread.currentThread().join();
-        } finally {
-            scheduler.shutdown();
-            LOG.info("Quartz server stopped.");
-        }
+        String config = System.getProperty("quartzConfig", DEFAULT_CONFIG);
+        final Scheduler scheduler = new StdSchedulerFactory(config).getScheduler();
+
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                try {
+                    LOG.info("Shutting down quartz server.");
+                    scheduler.shutdown();
+                } catch (SchedulerException e) {
+                    throw new RuntimeException("Failed to shutdown quartz server.", e);
+                }
+            }
+        });
+
+        LOG.info("Starting quartz server.");
+        scheduler.start();
+        Thread.currentThread().join();
     }
 }
