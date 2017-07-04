0b82aaecd64ced6830447aa32ca9248fca04a7a0
Zemian Deng <zemiandeng@gmail.com>
2017-07-04 11:59:09 -0400

    Add postgres db conn test

--- src/main/java/zemian/quartz/examples/DBConn.java
+++ src/main/java/zemian/quartz/examples/DBConn.java
@@ -1,5 +1,8 @@
 package zemian.quartz.examples;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 
@@ -7,10 +10,30 @@ import java.sql.DriverManager;
  * Created by zemian on 7/4/17.
  */
 public class DBConn {
+    private static Logger LOG = LoggerFactory.getLogger(DBConn.class);
+
     public static void main(String[] args) throws Exception {
+        postgres(args);
+        //hsqldb(args);
+    }
+
+    public static void postgres(String[] args) throws Exception {
+        Class.forName("org.postgresql.Driver" );
+        Connection conn = DriverManager.getConnection("jdbc:postgresql://localhost:5432/quartz", "postgres", "");
+        try {
+            LOG.info("DB Connection={}", conn);
+        } finally {
+            conn.close();
+        }
+    }
+
+    public static void hsqldb(String[] args) throws Exception {
         Class.forName("org.hsqldb.jdbc.JDBCDriver" );
-        Connection c = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost", "SA", "");
-        System.out.println(c);
-        c.close();
+        Connection conn = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost", "SA", "");
+        try {
+            LOG.info("DB Connection={}", conn);
+        } finally {
+            conn.close();
+        }
     }
 }
